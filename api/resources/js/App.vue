6da871789bcfa27121787e53c28291e0524c2ed6
evy-rodriguez03 <evyrodriguez03@gmail.com>
2025-03-12 17:43:47 -0600

    Implementacion de vista principal

---
+++ api/resources/js/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <div>
+      <router-view></router-view> <!-- Renderiza el componente basado en la ruta actual -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
+  <style>
+  /* Estilos globales de la aplicación */
+  body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  </style>
+  
---
+++ api/resources/js/components/MainView.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div class="main-view">
+      <h1>Bienvenido a la aplicación de gestión de tareas</h1>
+     
+      <p>Aquí irán los detalles de tus tareas y proyectos.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MainView',
+  };
+  </script>
+  
+  <style scoped>
+ 
+  .main-view {
+    text-align: center;
+    margin-top: 50px;
+  }
+  </style>
+  
